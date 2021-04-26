668f98981354cc0eb2dd0810f11a5f9b9755e0bd
Arjun C  Menon <55993011+justArno@users.noreply.github.com>
2021-04-25 22:02:10 -0400

    Add files via upload

---
+++ Code_Opt/input3.py
@@ -0,0 +1,33 @@
+import pandas
+x=2
+y=3
+z=x+y
+
+b=5
+a=5
+m=5
+
+
+while(b==y):
+	c=0
+	k=5
+	z=2
+	b=z
+	if(z==b):
+		c=1+b
+		b=4
+	else:
+		c=1+c
+	w=2
+
+#Comment
+if(x==y):
+	x=2
+else:
+	x=2
+
+if(x==y):
+	x=2
+else:
+	y=3
+
